1de8ce1a5d8c5df250d722c000c594d64e44a837
migara tennakoon <migaraten@gmail.com>
2025-02-17 10:53:43 +0200

    create categorys in backend

--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -6,10 +6,13 @@ import { ContentModule } from './content/content.module';
 import { TagsService } from './tags/tags.service';
 import { TagsController } from './tags/tags.controller';
 import { TagsModule } from './tags/tags.module';
+import { CategoriesService } from './categories/categories.service';
+import { CategoriesController } from './categories/categories.controller';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ContentModule, TagsModule],
-  controllers: [AppController, TagsController],
-  providers: [AppService, PrismaService, TagsService],
+  imports: [ContentModule, TagsModule, CategoriesModule],
+  controllers: [AppController, TagsController, CategoriesController],
+  providers: [AppService, PrismaService, TagsService, CategoriesService],
 })
 export class AppModule {}
